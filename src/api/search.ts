5e50c47eacd1889f8132e6a1f07747a760972865
Roi Talpaz <2651169+fokolo@users.noreply.github.com>
2024-09-17 12:44:57 +0300

    formatting

--- src/api/search.ts
+++ src/api/search.ts
@@ -9,18 +9,18 @@ export type PortEntity = {
 
 export default async function search(
   backendApiUrl: string,
-  searchQuery: object,
+  searchQuery: object
 ): Promise<PortEntity[]> {
   const response = await fetch(
     `${backendApiUrl}/api/proxy/getport/entities/search`,
     {
-      method: 'POST',
-      credentials: 'include',
+      method: "POST",
+      credentials: "include",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(searchQuery),
-    },
+    }
   );
   const json = await response.json();
 
